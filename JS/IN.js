5d66285fb5d90e3c5b24ae9051c2ce1f176506d2
Jacory Armstrong <jacoryarmstrong@gmail.com>
2022-08-23 17:05:04 -0400

    new pictures few new pages switch some pages around

---
+++ JS/IN.js
@@ -0,0 +1,19 @@
+function ShowMenu() {
+  let menu = document.querySelector(".container-aside"),
+      btnShowMenu = document.querySelector(".containerMain-ShowMenu");
+  menu.classList.toggle("showMenu");
+  if (btnShowMenu.innerHTML == "<i class=\"bi-list\"></i>")
+      btnShowMenu.innerHTML = "<i class=\"bi-x-lg\"></i>";
+  else
+      btnShowMenu.innerHTML = "<i class=\"bi-list\"></i>";
+}
+
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,5 @@ window.onclick = function(event) {
     }
   }
 }
+
+
