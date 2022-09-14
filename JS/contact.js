14bae1b321af00e9af4cdb1a7e4cfd8da97eade4
Jacory Armstrong <jacoryarmstrong@gmail.com>
2022-09-13 23:11:46 -0400

    started using sass new images changed a few colors

--- JS/contact.js
+++ JS/contact.js
@@ -17,3 +17,14 @@ window.onclick = function(event) {
     }
   }
 }
+function onlyNumberKey(evt) {
+          
+  // Only ASCII character in that range allowed
+  var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+      return false;
+  return true;}
+
+  function detailssubmit() {
+    alert("Your details were Submitted");
+}
